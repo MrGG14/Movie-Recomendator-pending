10891907c296734a198ff9b7abd84d698fbacc36
MrGG14 <nicovegamunoz1@gmail.com>
2022-04-24 23:57:38 +0200

    Update data_cleaning.py

--- data-manipulation/data_cleaning.py
+++ data-manipulation/data_cleaning.py
@@ -11,25 +11,22 @@ import os
 
 
 #%%
-
-os.chdir(r'C:\Users\nicov\OneDrive - Universidad Politécnica de Madrid\Escritorio\proyectos\recomendador_pelis')
-# print(os.getcwd())
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
 
 #%%
 # links_small_df = pd.read_csv('links_small.csv')
-ratings_small_df = pd.read_csv('ratings_small.csv')
-movies_metadata_df = pd.read_csv('movies_metadata.csv')
-keywords_df = pd.read_csv('keywords.csv')
+root = '../datasets/'
+ratings_small_df = pd.read_csv(root+'ratings_small.csv')
+movies_metadata_df = pd.read_csv(root+'movies_metadata.csv')
+keywords_df = pd.read_csv(root+'keywords.csv')
 
-#%%
-print(movies_metadata_df[movies_metadata_df['budget'] == '/ff9qCepilowshEtG2GYWwzt2bs4.jpg']['title'])
 #%%
 movies_metadata_df.drop(columns=['adult', 'belongs_to_collection', 'homepage','imdb_id', 'overview','poster_path','tagline','status','original_title','video', 'popularity','revenue','spoken_languages'],inplace=True) #se podria quitar 'title' en vez de 'original_title', depende del idioma del usuario
-
-
 movies_metadata_df.rename(columns={'id': 'movieId'},inplace=True)
 # pd.to_datetime(movies_metadata_df['release_date'])
-# movies_metadata_df['budget'] = movies_metadata_df['budget'].astype('int64', copy=False)
+movies_metadata_df['budget'] = movies_metadata_df['budget'].astype('int64', copy=False)
 movies_metadata_df['movieId'] = movies_metadata_df['movieId'].astype('string', copy=False)
 
 # to_float = ['popularity']
@@ -48,18 +45,14 @@ ratings_small_df['movieId'] = movies_metadata_df['movieId'].astype('string', cop
 
 full_df = pd.merge(ratings_small_df,movies_metadata_df, on='movieId')
 
-genres = full_df['genres'].tolist()
-genres[0][1]
 
-#%%
 for i in range(len(full_df['genres'])):
     genres = full_df['genres'][i]
-    ids = re.findall(r'\d+', genres)
-    print(full_df.loc[i]['genres'])
-    full_df.loc[i]['genres']=ids
-    # print(ids)
+    numbers = re.findall(r'\d+', genres)
+    ids = ','.join([n for n in numbers])
+    full_df.loc[i,'genres']=ids
         
-print(full_df['genres'])
+
 #%%
 print(movies_metadata_df.info())
 print(ratings_small_df.info())
