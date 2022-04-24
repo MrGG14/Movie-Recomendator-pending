ec4b273c970fc604821b39912173d7fbabda7090
MrGG14 <nicovegamunoz1@gmail.com>
2022-04-24 21:50:49 +0200

    user_scraping

--- filmaffinity/user_scraping.py
+++ filmaffinity/user_scraping.py
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Apr  4 12:52:44 2022
+Created on Mon Apr  4 03:52:44 2022
 
 @author: Nico
 """
 
 from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
-# import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-# import csv
 import os
 import pandas as pd
 import requests as req
@@ -23,7 +20,7 @@ os.chdir(dname)
 
    
 
-def scrap_movies_it(user_url,i):
+def scrap_user_it(user_url,i):
     df = pd.DataFrame({'title': [], 'date': [], 'avg_rating': [], 'my_rating': []})
     url = user_url + str(i)+'&orderby=0'
     driver.get(url)
@@ -49,15 +46,22 @@ def scrap_movies_it(user_url,i):
             df = df.append(new_row,ignore_index=True)
 
     if req.get(user_url + str(i+1)+'&orderby=0'):        
-        return pd.concat([df, scrap_movies_it(user_url,i+1)], ignore_index=True)
+        return pd.concat([df, scrap_user_it(user_url,i+1)], ignore_index=True)
     else: 
         print('Done!')
         return df
         
 
-def scrap_movies(user_url):
+def scrap_user(user_url):
     user_url = user_url + '&p='
-    return scrap_movies_it(user_url,1)
+    return scrap_user_it(user_url,1)
+
+
+def scrap_users(users_urls):
+    for user in users:              
+        df = scrap_user(users[user])
+        df.to_csv(str(user) + '.csv', index=False)
+
 
 
 
@@ -66,15 +70,9 @@ if __name__ == '__main__':
     driver = webdriver.Chrome(dname+'\chromedriver.exe')     
     
     user1 = 'https://www.filmaffinity.com/us/userratings.php?user_id=2683806'
-    user2 = 'https://www.filmaffinity.com/es/userratings.php?user_id=515396'
-    user3 = 'https://www.filmaffinity.com/es/userratings.php?user_id=6906529'
-    users = {'user1':user1, 'user2':user2, 'user3': user3}
+    users = {'user1': user1}
     
-    for user in users:        
-        df = scrap_movies(users[user])
-        df.to_excel(str(user) + '.xlsx', index=False)
-        # df.to_csv(str(user) + '.xlsx', index=False)
-
-
-
-
+    scrap_users(users)
+    
+    # df = scrap_user('https://www.filmaffinity.com/us/userratings.php?user_id=2683806') 
+        
