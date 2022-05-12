622d94c7af6d54b287a92971cdf0d207cb017301
MrGG14 <nicovegamunoz1@gmail.com>
2022-05-12 13:11:17 +0200

    Update data_cleaning.py

--- recomendator/data_cleaning.py
+++ recomendator/data_cleaning.py
@@ -35,9 +35,11 @@ movies_metadata_df['movieId'] = movies_metadata_df['movieId'].astype('string', c
 
 ratings_small_df.drop(columns=['timestamp'],inplace=True)
 ratings_small_df['movieId'] = ratings_small_df['movieId'].astype('string', copy=False)
+ratings_small_df['userId'] = ratings_small_df['userId'].astype('string', copy=False)
 
 full_df = pd.merge(ratings_small_df,movies_metadata_df, on='movieId')
-
+full_df['budget'] = full_df['budget'].astype('int', copy=False)
+full_df['original_language'] = full_df['original_language'].astype('string', copy=False)
 #dropping genre labels, keeping just its id
 # for i in range(len(full_df['genres'])):
 #     genres = full_df['genres'][i]
@@ -45,12 +47,19 @@ full_df = pd.merge(ratings_small_df,movies_metadata_df, on='movieId')
 #     ids = ','.join([n for n in numbers])
 #     full_df.loc[i,'genres']='[' + ids + ']'
 
-def genre_ids(x):
+def extract_ids(x):
     numbers = re.findall(r'\d+',x)
     ids = ','.join([n for n in numbers])
     return  list(ids.split(',')) 
 
-full_df['genres'] = full_df['genres'].apply(genre_ids)
+def extract_countryId(x):
+    numbers = re.findall(r"(iso_.*?)'",x)
+    ids = ','.join([n for n in numbers])
+    return  list(ids.split(',')) 
+
+full_df['genres'] = full_df['genres'].apply(extract_ids)
+full_df['production_companies'] = full_df['production_companies'].apply(extract_ids)
+full_df['production_countries'] = full_df['production_countries'].apply(extract_countryId)
 
 
 #create users_df
@@ -73,8 +82,8 @@ mlb = MultiLabelBinarizer()
 s = users_df['rated_movs']
 seen_movs = pd.DataFrame(mlb.fit_transform(s),columns=mlb.classes_, index=s.index)
 users_df = pd.merge(users_df,seen_movs, on='userId')
-
-
+# full_df[0:10].plot(kind='barh')
+# full_df.info()
 
 ''' 
 COSAS DE PRUEBASs[1]
